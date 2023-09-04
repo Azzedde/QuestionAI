30606d152389aa0c4cdbda05d16c7c91ea62bdb6
Azzedde <ja_aitsaid@esi.dz>
2023-09-04 17:06:23 +0100

    Initial Commit

---
+++ Home.py
@@ -0,0 +1,30 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Home",
+    page_icon="👋",
+)
+
+st.write("# Welcome to the Ultimate Knowledge Extraction Suite!👋")
+
+
+st.markdown(
+    """
+
+
+## 📚 Empowering Your Data Journey 🚀
+
+Dive deep into the vast ocean of information and extract meaningful insights with just a click. Whether it's a scholarly PDF, an insightful article, data-driven Excel spreadsheets, or even the colossal Google searches, this application ensures you get the best out of every byte.
+
+### Key Features:
+
+- 📄 PDF Insights: No need to comb through pages. Extract crucial knowledge in seconds.
+
+- 📰 Article Analysis: Understand the essence of articles without the fluff.
+
+- 🔍 Google Search Synthesis: Merge the vastness of Google search results into concise, actionable insights.
+
+- 📊 Excel & CSV Deciphering: Transmute rows & columns into understandable narratives.
+
+"""
+)
